259d2a59edccd96b6c2a382e92f849687829f1df
DevSeanFarkas <DevSeanFarkas@users.noreply.github.com>
2020-02-05 21:40:05 -0500

    You've Got Ass
    Smectum Rectum

---
+++ Assets/Present.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Present : MonoBehaviour
+{
+    private object ass;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetSumAss();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public object GetSumAss()
+    {
+        return ass;
+    }
+}
